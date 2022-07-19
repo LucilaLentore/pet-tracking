983b1abff1299a44f8c4a38d46b663601a6a8be8
Lula <lulalentore@gmail.com>
2022-07-18 22:49:11 -0300

    Add edit and delete buttons

--- src/components/PetDetail.jsx
+++ src/components/PetDetail.jsx
@@ -1,16 +1,43 @@
 const PetDetail = ({ patient }) => {
   const { name, owner, email, date, symptom } = patient;
-  return(
+  return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
-      <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""} <span className="font-normal normal-case">{name}</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""} <span className="font-normal normal-case">{name}</span>
+      </p>
 
-      <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {""} <span className="font-normal normal-case">{owner}</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}{" "}
+        <span className="font-normal normal-case">{owner}</span>
+      </p>
 
-      <p className="font-bold mb-3 text-gray-700 uppercase">Email: {""} <span className="font-normal normal-case">{email}</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""} <span className="font-normal normal-case">{email}</span>
+      </p>
 
-      <p className="font-bold mb-3 text-gray-700 uppercase">Fecha alta: {""} <span className="font-normal normal-case">{date}</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha alta: {""} <span className="font-normal normal-case">{date}</span>
+      </p>
 
-      <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {""} <span className="font-normal normal-case">{symptom}</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Síntomas: {""}{" "}
+        <span className="font-normal normal-case">{symptom}</span>
+      </p>
+
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 };
