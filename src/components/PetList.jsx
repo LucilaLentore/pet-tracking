839aceec0ad63aceb765ea6718ca34705eb56083
Lula <lulalentore@gmail.com>
2022-07-18 22:44:24 -0300

    Add conditional title for PetList component

--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -1,14 +1,33 @@
 import PetDetail from "./PetDetail";
 
 const PetList = ({ patients }) => {
-  return(
+  return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-scroll">
-      <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
-      <p className="text-lg mt-5 text-center mb-10">Administra tus {""} <span className="font-bold text-indigo-600">Pacientes y Citas</span></p>
+      {patients && patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            Listado de Pacientes
+          </h2>
+          <p className="text-lg mt-5 text-center mb-10">
+            Administra tus {""}{" "}
+            <span className="font-bold text-indigo-600">Pacientes y Citas</span>
+          </p>
 
-      {
-        patients.map(patient => <PetDetail key={patient.id} patient={patient} />)
-      }
+          {patients.map((patient) => (
+            <PetDetail key={patient.id} patient={patient} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            No hay Pacientes
+          </h2>
+          <p className="text-lg mt-5 text-center mb-10">
+            Comienza a agregar pacientes {""}{" "}
+            <span className="font-bold text-indigo-600">y aparecerán en este lugar</span>
+          </p>
+        </>
+      )}
     </div>
   );
 };
